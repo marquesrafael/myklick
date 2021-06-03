1582cebdfc87c46a7119e0342f77084ddd9ccf69
Rafael Marques <smarques.rafael@gmail.com>
2021-06-03 19:33:34 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>MyKlick - Em Breve</title>
+
+  <link rel="stylesheet" href="assets/dist/bootstrap/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/css/global.css" />
+  <link rel="shortcut icon" href="assets/img/favicons/icone.ico" />
+
+  <meta name="author" content="Rafael Marques" />
+  <meta name="contact" content="contato@rafaelmarques.net" />
+  <meta name="description" content="Nunca antes foi tão fácil COMPRAR - VENDER E ALUGAR Você pode fazer de qualquer lugar. Baixe nosso aplicativo AGORA! e nunca pare de ganhar dinheiro EXTRA.">
+  <meta name="keywords" content="myklick,fácil,comprar,vender">
+  <meta name="copyright" content="© MYKLICK 2021. Todos os direitos reservados">
+
+  <meta name="robots" content="index, follow" />
+  <meta name="googlebot" content="index, follow" />
+
+  <meta property="og:site_name" content="MYKLICK" />
+  <meta property="og:description" content="Nunca antes foi tão fácil COMPRAR - VENDER E ALUGAR Você pode fazer de qualquer lugar. Baixe nosso aplicativo AGORA! e nunca pare de ganhar dinheiro EXTRA." />
+  <meta property="og:title" content="MYKLICK" />
+  <meta property="og:url" content="https://myklick.com.br/" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="/img/logo.png" />
+  <meta property="og:image:mime" content="image/png" />
+  <meta property="og:image:width" content="492" />
+  <meta property="og:image:height" content="589" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:title" content="MYKLICK" />
+  <meta name="twitter:image" content="/img/logo.png" />
+  <meta name="twitter:description" content="Nunca antes foi tão fácil COMPRAR - VENDER E ALUGAR Você pode fazer de qualquer lugar. Baixe nosso aplicativo AGORA! e nunca pare de ganhar dinheiro EXTRA." />
+
+</head>
+
+<body id="top">
+  <nav class="navbar transparent navbar-inverse nav-masthead">
+    <div class="container-fluid inner">
+      <a class="navbar-brand">Em Breve</a>
+      <form class="d-flex">
+        <a class="nav-link nav-social" href="https://www.facebook.com/myklickbrasil" title="Facebook"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+        <a class="nav-link nav-social" href="https://www.instagram.com/myklickbrasil/?hl=pt-br" title="Instagram"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+      </form>
+    </div>
+  </nav>
+
+  <div class="cover">
+    <img src="assets/img/logo.png" class="logo img-fluid" />
+    <!--<h1 class="cover-heading">The adventure Begins</h1>-->
+    <p class="lead cover-copy">
+      Nunca antes foi tão fácil COMPRAR, VENDER e ALUGAR!<br />
+      Você pode fazer de qualquer lugar.<br />
+      <!-- Baixe nosso aplicativo <strong class="fu">AGORA</strong> e nunca pare de ganhar <strong class="am">dinheiro EXTRA</strong>.</p>-->
+      Assine nossa newsletter e seja um dos primeiros a utilizar!
+    </p>
+    <br />
+    <br />
+    <button type="button" data-bs-toggle="modal" data-bs-target="#exampleModal" class="btn btn-lg btn-default btn-notify btn-lg">Me avise</button>
+  </div>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Me avise</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Seu nome</label>
+            <input type="text" class="form-control" placeholder="Seu nome" name="nome">
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Seu email</label>
+            <input type="email" class="form-control" placeholder="email@email.com" name="email">
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+          <button type="button" class="btn btn-primary">Cadastrar</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src="assets/dist/jquery/jquery-3.6.0.min.js"></script>
+  <script src="assets/dist/vuejs/vuejs.js"></script>
+  <script src="assets/dist/bootstrap/js/bootstrap.min.js"></script>
+  <script src="https://kit.fontawesome.com/2c3fcb53c8.js" crossorigin="anonymous"></script>
+  <script src="assets/js/global.js"></script>
+</body>
+
+</html>
